1eb94189d4958676863fff5f0e2d15036bf685bf
fanisah <fanisah@mail.ugm.ac.id>
2025-09-30 11:17:57 +0700

    Add User.js and Request.js models

---
+++ src/models/Request.js
@@ -0,0 +1,34 @@
+// models/Request.js
+const mongoose = require('mongoose');
+
+const requestSchema = new mongoose.Schema({
+  projectId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Project',
+    required: true
+  },
+  requesterId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  message: {
+    type: String,
+    required: true
+  },
+  approved: {
+    type: Boolean,
+    default: false
+  },
+  approvedAt: {
+    type: Date
+  },
+  proposalLink: { type: String, default: null } 
+}, {
+  timestamps: true
+});
+
+// Ensure one request per user per project
+requestSchema.index({ projectId: 1, requesterId: 1 }, { unique: true });
+
+module.exports = mongoose.model('Request', requestSchema);
